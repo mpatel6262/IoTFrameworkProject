afd2f635a1097256e9b27f8f101e3dcf4871d2a5
SOMATUMMALA21 <41241108+SOMATUMMALA21@users.noreply.github.com>
2023-07-28 21:28:59 -0700

    Create mqttSubscriberAndKafkaPublisher.py
    Initial commit

---
+++ mqttSubscriberAndKafkaPublisher.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+from pykafka import KafkaClient
+import time
+
+mqttBroker = "mqtt.eclipseprojects.io"
+client = mqtt.Client("Smartphone")
+client.connect(mqttBroker)
+
+kafka_client = KafkaClient(hosts="localhost:9092")
+kafka_topic = kafka_client.topics['temperature']
+kafka_producer = kafka_topic.get_sync_producer()
+
+def on_message(client, userdata, message):
+    msg_payload = str(message.payload.decode("utf-8"))
+    print("Received message: ", msg_payload)
+    kafka_producer.produce(msg_payload.encode('ascii'))
+    print("KAFKA: Just published " + msg_payload + " to topic temperature")
+
+client.loop_start()
+client.subscribe("TEMPERATURE")
+client.on_message = on_message
+time.sleep(30)
+client.loop_end()
