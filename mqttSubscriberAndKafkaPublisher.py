873f654e238e3155e7c898b3e2ab66fae01dd376
Pranay Tiru <pranaytiru02@gmail.com>
2023-08-18 23:31:04 -0700

    Updated Publisher

--- mqttSubscriberAndKafkaPublisher.py
+++ mqttSubscriberAndKafkaPublisher.py
@@ -7,17 +7,17 @@ client = mqtt.Client("Smartphone")
 client.connect(mqttBroker)
 
 kafka_client = KafkaClient(hosts="localhost:9092")
-kafka_topic = kafka_client.topics['temperature']
+kafka_topic = kafka_client.topics['TISENSORTAGDATA']
 kafka_producer = kafka_topic.get_sync_producer()
 
 def on_message(client, userdata, message):
     msg_payload = str(message.payload.decode("utf-8"))
     print("Received message: ", msg_payload)
     kafka_producer.produce(msg_payload.encode('ascii'))
-    print("KAFKA: Just published " + msg_payload + " to topic temperature")
+    print("KAFKA: Just published " + msg_payload + " to topic TISENSORTAGDATA")
 
 client.loop_start()
-client.subscribe("TEMPERATURE")
+client.subscribe("TISENSORTAGDATA")
 client.on_message = on_message
 time.sleep(30)
-client.loop_end()
+client.loop_stop()
