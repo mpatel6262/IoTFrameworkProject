b6a459eed633f90f3bc189ba6a8b84e8521f22d3
SOMATUMMALA21 <somatummala03@gmail.com>
2023-09-10 02:38:00 -0700

    Aggregate logic

--- pySparkJob.py
+++ pySparkJob.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, window, avg
 
 # Step 1: Create a Spark session
 spark = SparkSession.builder \
@@ -21,9 +22,17 @@ kafkaStream = spark.readStream \
 kafkaStream = kafkaStream.selectExpr("CAST(value AS STRING)")
 
 # Step 5: Define your processing logic (if needed)
-# For this example, we'll simply write the data to another Kafka topic.
-query = kafkaStream.writeStream \
-    .outputMode("append") \
+# For this example, we'll calculate the average for each specific topic.
+aggregatedStream = kafkaStream.selectExpr(
+    "CAST(value AS STRING) as message",
+    "split(value, ',')[0] as topic",
+    "cast(split(value, ',')[1] as int) as value"
+).groupBy("topic").agg(avg("value").alias("average_value"))
+
+# Step 6: Write the average values to another Kafka topic
+query = aggregatedStream.selectExpr("topic", "cast(average_value as string) as value") \
+    .writeStream \
+    .outputMode("complete") \
     .format("kafka") \
     .option("kafka.bootstrap.servers", kafka_brokers) \
     .option("topic", kafka_output_topic) \
